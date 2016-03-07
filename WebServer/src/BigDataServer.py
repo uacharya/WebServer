02ba72aef0b792445312f3d85b506265b0cfa063
Ujjwal Acharya <uzwal47@gmail.com>
2016-03-06 18:20:03 -0700

    created web server at local host

--- WebServer/src/BigDataServer.py
+++ WebServer/src/BigDataServer.py
@@ -3,9 +3,22 @@ Created on Feb 29, 2016
 
 @author: Uzwal
 '''
-def doSomething():
-    print("hello world");
+import os
+import SimpleHTTPServer
+import SocketServer
+
+
+def runServer():
+    try:
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler # http request and response handler
+        httpServer = SocketServer.TCPServer(("127.0.0.1", 8080), Handler)
+        print("web server is running")
+        httpServer.serve_forever()
+    except KeyboardInterrupt:
+        print("server is closing")
+        httpServer.socket.close();
+        
 
 if __name__ == '__main__':
-    doSomething()
+    runServer()
     
