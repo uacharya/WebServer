7429369dcf24c7f12cb9f72742aac8290e41e482
Ujjwal Acharya <uzwal47@gmail.com>
2016-05-26 16:47:09 -0600

    Starting creating final dataset for visualizing line graph

---
+++ WebServer/src/PreProcessing_Data/final_global_dataset.py
@@ -0,0 +1,20 @@
+'''
+Created on May 26, 2016
+
+@author: Ujjwal Acharya
+'''
+from Create_required_text_file import Data;
+
+if __name__ == '__main__':
+    
+    file_path_of_total_data = "C:/Users/walluser/combined.txt";
+    
+    file_path_of_country_and_station_name = "C:/Users/walluser/Global_Weather_Dataset/station_names_into_countries.txt";
+    
+    new_object = Data();
+    
+    new_object.create_only_required_data(file_path_of_total_data, file_path_of_country_and_station_name);
+    
+    
+    
+    
