216e3948977c52ec09b321f97f6d7d62d7ab761f
Ujjwal Acharya <uzwal47@gmail.com>
2017-02-28 13:49:05 -0700

    A blue print of running actual wind flow simulation application

--- WebServer/src/TestingSpark.py
+++ WebServer/src/TestingSpark.py
@@ -5,58 +5,71 @@ Created on Mar 10, 2016
 '''
 
 from pyspark import SparkConf, SparkContext;
-import json, operator;
-import os
 
 
-def make_part_filter(index):
-    def part_filter(split_index, iterator):
-        if split_index == index:
-            for element in iterator:
-                for content in element:
-                    yield str(content);
+
+
+
+
+def create_required_datewise_data(line):
     
-    return part_filter;
-
-def to_CSV(data,header):
-    data = header+data;
-    return "\n".join(data);
-
-def write_data_to_file(index,data):
-    file_output_path =  "/Users/Uzwal/Desktop/output"+index+".csv";
-    file_write = open(file_output_path,"w");
-    for line in data:
-        file_write.write(line)
+    data = line.split("\t");
     
-        
+    data_dictionary = {"Station":str(data[0]), "temperature":str(data[2]), "pressure":str(data[5]), "Density":str(data[6]), "Wind_Velocity":str(data[8]),
+                       "Latitude":str(data[15]), "Longitude":str(data[16]), "Elevation":str(data[17])};
+    key = str(data[1]);
+    
+    return (key, data_dictionary);
+
+def collecting_data(split_index, iterator):
+    if(split_index == 0):
+        for data in iterator:
+            yield str(data);
+    
+            
 if __name__ == '__main__':
     
     # configure the spark environment
-    sparkConf = SparkConf().setAppName("WordCount").setMaster("local[4]")
+    sparkConf = SparkConf().setAppName("Creating Data").setMaster("local[4]");
     sc = SparkContext(conf=sparkConf);
-        
-#     #The wordcount Spark Program
-#     text_file = sc.textFile(os.environ['SPARK_HOME']+"/README.md");
-#     print(text_file);
-#     word_count = text_file.flatMap(lambda line:line.split()).map(lambda word:(word,1)).reduceByKey(operator.add);
-#        
-#     for wc in word_count.collect():
-#         print wc
-    data = sc.textFile("/Users/Uzwal/Desktop/SVMDataSet.csv").map(lambda data:data.split("\n")).collect();
-    header_of_file = data[0];
-    data_from_file = data[1:];
-    distributedDataset = sc.parallelize(data_from_file, 3);
-    local_data_taken_from_distribution = distributedDataset.getNumPartitions();
-    index = 1;
     
-    for part in range(local_data_taken_from_distribution):
-        part_rdd = distributedDataset.mapPartitionsWithIndex(make_part_filter(part),True);
-        data_for_one_node = part_rdd.collect();
-        convert_data_to_required_file = to_CSV(data_for_one_node,header_of_file);
-        print(" the data for %s node is : %s" %(part,convert_data_to_required_file));  
-          
+    distributed_dataset = sc.textFile("/Users/Uzwal/Desktop/preprocessed_combined.txt");
+    
+    header = distributed_dataset.first();
+    
+    distributed_dataset = distributed_dataset.filter(lambda d: d!=header);
+    
+    data_in_required_format = distributed_dataset.map(create_required_datewise_data);
+    
+#     data_in_required_format.persist();
      
-    print("the number of partitions are %s"%local_data_taken_from_distribution);
+    broadcast_data = data_in_required_format.collect();
+     
+    broadcast_variable = sc.broadcast(broadcast_data);
+     
+    hash_partitions = data_in_required_format.partitionBy(4);
+     
+    output = hash_partitions.mapPartitionsWithIndex(collecting_data).collect();
+    
+    print("i am the size"+str(len(output)));
+    
+    for item in output:
+        print(item);
+    
+     
+    print("the keys are" + str(data_in_required_format.getNumPartitions()));
+    
+    print(broadcast_variable.value[1]);
+    
+    
+    
+    
+    
+    
+#     for line in collected_data:
+#         print(line + "\t" + str(collected_data[line]));
+    
+    
     
     sc.stop();
 
