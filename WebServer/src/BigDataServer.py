afc05a3e6705225184836f1fafbb86cb80fad5f2
Ujjwal Acharya <uzwal47@gmail.com>
2017-05-02 21:49:26 -0600

    A web server with post method as well

--- WebServer/src/BigDataServer.py
+++ WebServer/src/BigDataServer.py
@@ -3,17 +3,17 @@ Created on Feb 29, 2016
 
 @author: Ujjwal Acharya
 '''
-import os
+import os,ast;
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
 
 # Create custom HTTPRequestHandler class
 class customHTTPRequestHandler(BaseHTTPRequestHandler):
-    # method for handling the http request from the client
+    # method for handling the http get request from the client
     def do_GET(self):
-        output_directory = "/Users/Uzwal/Documents/workspace/bilevel/WebContent";
+        output_directory = "C:/Users/walluser/Desktop/testing";
         try:
-            #printing headers of connection made
-            print(self.headers);
+            
             file_to_send = open(output_directory + self.path, "r");  # opening the file to send
             #send a connection was made response code
             self.send_response(200,"ok");
@@ -21,12 +21,33 @@ class customHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers();
             # sending file to client via output stream
             self.wfile.write(file_to_send.read()) 
+            self.wfile.close();
             file_to_send.close();
             return;
         except IOError:
             self.send_error(404, 'file not found')
             file_to_send.close()
             return
+    
+    #method for handling http post request  
+    def do_POST(self):
+        try:
+            content_len = int(self.headers['content-length']);
+            posted_message = self.rfile.read(content_len);
+            required_data_date = ast.literal_eval(posted_message)["date"];
+            #send a connection was made response code
+            self.send_response(200,"ok");
+            self.send_header("Access-Control-Allow-Origin","null");
+            self.end_headers();
+            if(os.path.isfile("C:/Users/walluser/Desktop/testing/data"+str(required_data_date)+".csv")):
+                self.wfile.write("Ready");
+                self.wfile.close();
+            else:
+                self.wfile.write("Not Ready");
+                self.wfile.close();   
+        except:
+            self.send_error(404,"file not found here i was found");
+            return;
      
 
 def runServer():
@@ -51,7 +72,7 @@ def sendFile():
         
 
 if __name__ == '__main__':
-    sendFile();
+#     sendFile();
     runServer();
         
     
